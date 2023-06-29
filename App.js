24f28730990c195da79bbb88065da0d0122b5f34
vantungdz <dovantung1000@gmail.com>
2023-06-29 19:42:31 +0700

    add button

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button,Linking  } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Nhan nut de mua hang</Text>
+      <Button title='Mua hang' onPress={ ()=>{ Linking.openURL('https://google.com')}} />
       <StatusBar style="auto" />
     </View>
   );
