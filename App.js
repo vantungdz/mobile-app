5251e83837d97002eb2e0f199151dcd9e7b5bd54
vantungdz <dovantung1000@gmail.com>
2023-06-29 19:58:19 +0700

    add background

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button,Linking  } from 'react-native';
+import React from 'react';
+import {ImageBackground, StyleSheet, Text, View , Button , Linking} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Nhan nut de mua hang</Text>
+const image = {uri: 'https://thiepmung.com/images/theme/ngam-va-tai-hinh-nen-dep-cho-may-tinh-556444149b46f6.jpg'};
+
+const App = () => (
+  <View style={styles.container}>
+    <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+      <Text style={{textAlign:'center' , marginBottom: 10 , fontSize: 20}}>Nhan de chuyen toi trang mua hang</Text>
       <Button title='Mua hang' onPress={ ()=>{ Linking.openURL('https://google.com')}} />
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    </ImageBackground>
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  image: {
+    flex: 1,
     justifyContent: 'center',
   },
 });
+
+export default App;
